2ae02ec12c9654c354b3960b589405d9a3794adc
waterwoodwind <469798015@qq.com>
2017-08-14 09:43:21 +0800

    图片文件命名优化

--- hr_pyautogui/main.py
+++ hr_pyautogui/main.py
@@ -4,20 +4,20 @@ import time
 
 def put_a_day(human_number):
     pyautogui.PAUSE = 1
-    number_locate = pyautogui.locateCenterOnScreen('number.png')
+    number_locate = pyautogui.locateCenterOnScreen('1_number.png')
     pyautogui.moveTo(number_locate)
     pyautogui.click()
 
     pyautogui.typewrite(human_number, interval=0.25)
-    query_locate = pyautogui.locateCenterOnScreen('query.png')
+    query_locate = pyautogui.locateCenterOnScreen('2_query.png')
     pyautogui.moveTo(query_locate)
     pyautogui.click()
 
-    com_box_locate = pyautogui.locateCenterOnScreen('com_box.png')
+    com_box_locate = pyautogui.locateCenterOnScreen('3_com_box.png')
     pyautogui.moveTo(com_box_locate)
     pyautogui.click()
 
-    chose_locate = pyautogui.locateCenterOnScreen('chose.png')
+    chose_locate = pyautogui.locateCenterOnScreen('4_chose.png')
     pyautogui.moveTo(chose_locate)
     pyautogui.click()
     pyautogui.keyDown('pagedown')
